658300c79c514ec674de019e264e24b379418e37
tmaraki <toshiemaraki@gmail.com>
2024-01-25 21:56:49 -0500

    updated index.js with basic js coding and muted notes and updated README file with sections and placeholders

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,72 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+// generateMarkdown
 
 // TODO: Create an array of questions for user input
-const questions = [];
 
+// const generateREADME = ({ project, Description, Installation, Usage, Contributing, Tests, License}) => {}
+
+
+const questions = [
+        {
+            type: 'input',
+            name: 'project',
+            message: 'What is the title of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Write a description of your project',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Write installation instructions',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Write usage information here'
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'What are the contribution guidelines?'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What are the test instructions?'
+        },
+        {
+            type: 'dropdown',
+            name: 'license',
+            message: 'Select a license',
+            choices: []
+        },
+        {
+            type: 'input',
+            name: 'GitHub',
+            message: 'Copy your GitHub profile link here'
+        },
+        {
+            type: 'input',
+            name: 'contact',
+            message: 'Enter your email address'
+        }
+    ]
+inquirer
+    .prompt(questions)
+    .then((data) => {
+        const dataPageContent = generateREADME(data);
+
+        fs.writeFile('README.md', dataPageContent, (err) =>
+        err ? console.log(err) : console.log('Successfully created index.html!')
+        );
+    })
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
--- README.md
+++ README.md
@@ -1,3 +1,43 @@
 # Professional README Generator Starter Code
 
 [How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+
+## ${project}
+
+# Description
+${description}
+
+# Table of Contents
+[Description]
+[Installation]
+[Usage]
+[Contributing]
+[Tests]
+[License]
+[Questions]
+
+
+# Installation 
+${installation}
+
+
+# Usage
+${usage}
+
+
+# Contributing
+${contributing}
+
+
+# Tests
+${tests}
+
+
+# License
+${license}
+
+
+# Questions
+For any questions, you can email me at ${contact}
+
+${GitHub}
