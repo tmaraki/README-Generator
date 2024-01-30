4cff99ddc8d56549ebf1d59d8b42edcdef533898
tmaraki <toshiemaraki@gmail.com>
2024-01-30 11:32:56 -0500

    previously deleted gitignore file, so added it back in without changing content. Modified index.js and generatMarkDown.js files to properly format the md file once generatMarkDown function is called

--- Develop/index.js
+++ Develop/index.js
@@ -1,146 +1,84 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-
-// const generateMarkdown = async (data) => {
-//     try {
-//       // Read the Markdown template file
-//       const template = await fs.promises.readFile('README.md', 'utf-8');
-  
-//       // Replace the placeholders in the template with the provided values
-//       const markdownContent = template
-//         .replace('{{project}}', data.project)
-//         .replace('{{Description}}', data.description)
-//         .replace('{{Installation}}', data.installation)
-//         .replace('{{Usage}}', data.usage)
-//         .replace('{{Contributing}}', data.contributing)
-//         .replace('{{Tests}}', data.tests)
-//         .replace('{{License}}', data.license);
-  
-//       // Return the generated Markdown content
-//       return markdownContent;
-//     } catch (error) {
-//       console.error('Error reading or generating Markdown:', error);
-//       return '';
-//     }
-//   };
-
-  const generateMarkdown = ({ project, Description, Installation, Usage, Contributing, Tests, License, Contact, GitHub, Repo}) => {
-    // Generate the Markdown content 
-    const markdownContent = `
-    # ${project}
-
-    ## Description
-        ${Description} 
-        
-    ## Table of Contents
-    [Description](${Repo}/blob/main/README.md#description)
-    
-    [Installation](https://github.com/tmaraki/potential-enigma/blob/main/README.md#installation)
-    
-    [Usage](https://github.com/tmaraki/potential-enigma/blob/main/README.md#usage)
-    
-    [Contributing](https://github.com/tmaraki/potential-enigma/blob/main/README.md#contributing)
-    
-    [Tests](https://github.com/tmaraki/potential-enigma/blob/main/README.md#tests)
-    
-    [License](https://github.com/tmaraki/potential-enigma/blob/main/README.md#license)
-    
-    [Questions](https://github.com/tmaraki/potential-enigma/blob/main/README.md#questions)
-        
-        
-    ## Installation 
-        ${Installation}
-        
-        
-    ## Usage
-        ${Usage}
-        
-        
-    ## Contributing
-        ${Contributing}
-        
-        
-    ## Tests
-        ${Tests}
-        
-        
-    ## License
-        ${License}
-        
-        
-    ## Questions
-        For any questions, you can email me at ${Contact}
-        
-        ${GitHub}
-    `;
-  
-    // Return the generated Markdown content
-    return markdownContent;
-  };
-
+const generateMarkdown = require('./utils/generateMarkdown')
 
 const questions = [
         {
             type: 'input',
             name: 'project',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project?',
+            validate: your_Input => {
+                if (your_Input) {
+                    return true;
+                } else {
+                    console.log('Enter a title to continue!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
-            name: 'Description',
+            name: 'description',
             message: 'Write a description of your project:',
         },
         {
             type: 'input',
-            name: 'Installation',
-            message: 'What are the installation instructions?', 
+            name: 'installation',
+            message: 'How do users install your project?', 
         },
         {
             type: 'input',
-            name: 'Usage',
-            message: 'Write usage information here:'
+            name: 'usage',
+            message: 'How do you use this project?'
         },
         {
             type: 'input',
-            name: 'Contributing',
-            message: 'What are the contribution guidelines?'
+            name: 'contributing',
+            message: 'How can users contribute to this project?'
         },
         {
             type: 'input',
-            name: 'Tests',
-            message: 'What are the test instructions?'
+            name: 'tests',
+            message: 'How do users test the project?'
         },
         {
             type: 'checkbox',
-            name: 'License',
+            name: 'license',
             message: 'Select a license:',
-            choices: ['None', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense']
+            choices: ['None', 'Apache 2.0', 'BSD 2-Clause License', 'Boost Software License 1.0', 'CC0 1.0', 'Eclipse Public License 1.0', 'GNU AGPL v3.0', 'GNU LGPL v3', 'GNU GPL v3.0', 'MIT', 'Mozilla Public License 2.0', 'The Unlicense']
         },
         {
             type: 'input',
             name: 'GitHub',
-            message: 'Copy your GitHub profile link here:'
+            message: 'Copy your GitHub profile link here:',
+            validate: GitHub_Input => {
+                if (GitHub_Input) {
+                    return true;
+                } else {
+                    console.log('Enter a link to your GitHub profile to continue!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
-            name: 'Repo',
-            message: 'Copy your project repo link here:'
-        },
-        {
-            type: 'input',
-            name: 'Contact',
-            message: 'Enter your email address:'
+            name: 'contact',
+            message: 'Enter your email address:',
+            validate: contact_input => {
+                if (contact_input) {
+                    return true;
+                } else {
+                    console.log('Enter an email address to continue!');
+                    return false;
+                }
+            }
         }
     ]
-inquirer
-    .prompt(questions)
-    .then((questions) => {
-        const dataPageContent = generateMarkdown(questions);
 
-// function to write README file
-function writeToFile(README, questions) {
-    fs.writeFile(README, JSON.stringify(questions, null, '\t'), (err) => {
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
       if (err) {
         console.log(err);
       } else {
@@ -148,13 +86,15 @@ function writeToFile(README, questions) {
       }
     });
   }
-        writeToFile('README.md', dataPageContent);
-    })
 
-  
+// function to initialize app: function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function(userInput) {
+        console.log(userInput)
+        writeToFile("README.md", generateMarkdown(userInput));
+    });
+};
 
-// TODO: Create a function to initialize app
-// function init() {}
-
-// Function call to initialize app
-// init();
+// function call to initialize app: init();
+init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,32 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (data.license === 'None') {
+    badge = '';
+  } else if (data.license === 'Apache 2.0') {
+    badge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+  } else if (data.icense === 'Boost Software License 1.0') {
+    badge = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
+  } else if (data.license === 'BSD 2-Clause License') {
+    badge = '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)';
+  } else if (data.license === 'CC0 1.0') {
+    badge = '[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)';
+  } else if (data.license === 'Eclipse Public License 1.0') {
+    badge = '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)';
+  } else if (data.license === 'GNU AGPL v3.0') {
+    badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+  } else if (data.license === 'GNU LGPL v3') {
+    badge = '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)';
+  } else if (data.license === 'GNU GPL v3.0') {
+    badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+  } else if (data.license === 'MIT') {
+    badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  } else if (data.license === 'Mozilla Public License 2.0') {
+    badge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+  } else if (data.license === 'The Unlicense') {
+    badge = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
+  } 
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -11,10 +37,51 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+function generateMarkdown(data) {
+  return `# ${data.project}
+
+## License
+${data.license}
+
+## Description
+${data.Description}
+
+## Table of Contents
+  [Description](#description)
+
+  [Installation](#installation)
+
+  [Usage](#usage)
+
+  [Contributing](#contributing)
+
+  [Tests](#tests)
+
+  [License](#license)
+
+  [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## License
+Licenses this application is covered under: ${data.license}
+
+## Questions
+For any questions, you can email me at ${data.contact}
+
+Here is a link to my GitHub: ${data.GitHub}`;
+
 }
 
-module.exports = generateMarkdown;
+// module.exports = generateMarkdown;
